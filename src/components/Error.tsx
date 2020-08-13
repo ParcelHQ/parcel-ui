87cdd8c8b168122b7f86d8f4dc6b7bd8239c5bf1
tarun1475 <tarunkumargupta14@gmail.com>
2020-08-13 16:46:43 +0530

    error message changed

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -10,7 +10,7 @@ function getErrorMessage(error: Error) {
   if (error instanceof NoEthereumProviderError)
     return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.';
   else if (error instanceof UnsupportedChainIdError)
-    return "You're connected to an unsupported network.";
+    return "Please connect to rinkeby testnet";
   else if (error instanceof UserRejectedRequestError)
     return 'Please authorize this website to access your Ethereum account.';
   else {
